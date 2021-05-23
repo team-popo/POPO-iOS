c13b24a2a4a3d77b2320e28fe5613552dd7b1221
iamcho2 <thatgrass@naver.com>
2021-05-23 20:18:32 +0900

    [#6] CoverCalender 뷰
    ### Description
    - 컴포넌트 배치
    - image example
    - Const 추가
    
    ### Related Issue
    close #6

---
+++ popo/popo/Resources/Constant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  popo
+//
+//  Created by 초이 on 2021/05/23.
+//
+
+import UIKit
+
+extension Const {
+    struct ViewController {
+        
+        struct Identifier {
+            static let loginMethod = "LoginMethodViewController"
+            static let coverCalendar = "CoverCalendarViewController"
+            static let coverUserPhoto = "CoverUserPhotoViewController"
+        }
+        
+    }
+}
